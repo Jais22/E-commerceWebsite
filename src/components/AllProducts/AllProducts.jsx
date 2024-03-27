e1e12c75077ebc8614916fefa7443651b19a8bd2
Jais22 <prachijaiswal438@gmail.com>
2024-03-27 17:09:41 +0530

    Add file

--- src/components/AllProducts/AllProducts.jsx
+++ src/components/AllProducts/AllProducts.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import shop from "../../assets/shop.jpeg"
 import { Link } from 'react-router-dom'
-import AllProductShimmer from '../AllProductShimmer/AllProductShimmer'
 
 //https://dummyjson.com/products/categories
 
